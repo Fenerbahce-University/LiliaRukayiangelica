e42ed34b2f35d2abac4b53e4bb277519dd477f14
Angelica3-3 <angelica.bankumuna@stu.fbu.edu.tr>
2026-01-14 16:15:15 +0000

    Add project structure and basic C++ files

---
+++ cpp/include/db.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void testConnection() {
+    std::cout << "Database connection test" << std::endl;
+}
---
+++ cpp/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "C++ application is running" << std::endl;
+    return 0;
+}
